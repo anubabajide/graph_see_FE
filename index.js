8311ee82e0b074b966f4d287bcfc4218d141c7ee
Sadiq Aderinto <sa.ayinda@gmail.com>
2021-01-01 21:31:36 +0100

    added lines

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class NodeClass {
     static increaseCount() {
         this.count += 1;
     }
-    static create(){
+    static createNode(){
         const div = document.createElement('div');
         div.id = `node-${this.count}`;
         div.className = "node";
@@ -11,23 +11,37 @@ class NodeClass {
         this.increaseCount();
         return div;
     }
-
-    constructor(x=50, y=50) {
-        this.node = this.constructor.create();
+    
+    constructor(x=50, y=50, neighbors = []) {
+        this.node = this.constructor.createNode();
+        
         // console.log({height:this.node.clientHeight})
+        this.x = x;
+        this.y = y;
         this.node.style.left = `calc(${x}vw - ${this.node.clientWidth/2}px)`;
         this.node.style.top = `calc(${y}vh  - ${this.node.clientHeight/2}px)`;
+        this.createConnections(neighbors)
     }
     
-    connect(nodeB) {
-        
+    createConnections(nodes){
+        console.log(nodes)
+        for (let idx in nodes){
+            this.connect(nodes[idx]);
+        }
+    }
+    connect(node) {
+        const line = `
+        <line x1="calc(${this.x}vw)" y1="calc(${this.y}vh)" x2="calc(${node.x}vw)" y2="calc(${node.y}vh)" stroke="black"/>
+        `
+        document.querySelector("#graph").insertAdjacentHTML("beforeEnd", line);
+
     }
 }
 
 function createNode() {
-    const newNode = new NodeClass();
     const newNode1 = new NodeClass(70, 70);
     const newNode2 = new NodeClass(70, 30);
     const newNode3 = new NodeClass(30, 30);
     const newNode4 = new NodeClass(30, 70);
+    const newNode = new NodeClass(undefined, undefined,[newNode1, newNode2, newNode3, newNode4]);
 }
